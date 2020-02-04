4a67789c99f4b3c11e05254a2eb0902ac68e1aed
Thierry ZENNARO <tzen@hotmail.fr>
2020-02-04 20:12:11 +0100

    Gestion des réservations (base)

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -180,4 +180,15 @@ class AccountController extends AbstractController
             'user' => $this->getUser()
         ]);
     }
+
+    /**
+    * Permet d'afficher la liste des réservations faits par l'utilisateur
+    *
+    * @Route("/account/bookings", name="account_bookings")
+    *
+    * @return Response
+    */
+    public function bookings() {
+        return $this->render('account/bookings.html.twig');
+    }
 }
---
+++ src/Controller/BookingController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Booking;
+use App\Form\BookingType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BookingController extends AbstractController
+{
+    /**
+     * @Route("/ads/{slug}/book", name="booking_create")
+     * @IsGranted("ROLE_USER")
+     */
+    public function book(Ad $ad, Request $request, EntityManagerInterface $manager)
+    {
+    	$booking = new Booking();
+		$form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+
+            $booking->setBooker($user)
+                    ->setAd($ad);
+
+            //Si les dates ne sont pas disponibles, message d'erreur
+            if (!$booking->isBookableDates()) {
+                $this->addFlash(
+                    'warning',
+                    "Les dates que vous avez choisi ne peuvent être réservées : elles sont déjà prises."
+                );
+            } else {
+                //Sinon enregistrement et redirection
+                $manager->persist($booking);
+                $manager->flush();
+
+                //return $this->redirectToRoute('booking_success', ['id' => $booking->getId()]);
+                return $this->redirectToRoute('booking_show', ['id' => $booking->getId(), 'withAlert' => true]);
+            }
+        }
+
+        return $this->render('booking/book.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+    * Permet d'afficher la page d'une réservation
+    *
+    * @Route("/booking/{id}", name="booking_show")
+    *
+    * @param Booking $booking
+    * @return Response
+    */
+    public function show(Booking $booking) {
+        return $this->render('booking/show.html.twig', [
+            'booking' => $booking
+            ]);
+    }
+}
